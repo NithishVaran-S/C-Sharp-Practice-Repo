b6ab486738d5339cd97c961fb21473ece877de6d
NithishVaran-S <133568626+NithishVaran-S@users.noreply.github.com>
2026-05-08 23:43:43 +0530

    Time: 37 ms (12.32%) | Memory: 49.1 MB (70.02%) - LeetSync

--- 1-two-sum/two-sum.cs
+++ 1-two-sum/two-sum.cs
@@ -1,6 +1,6 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        List<int> sol = new List<int>();
+        
         for (int i = 0; i < nums.Length; i++)
         {
             for (int j = i+1; j < nums.Length; j++)
@@ -8,12 +8,10 @@ public class Solution {
                 
                 if (nums[i] + nums[j] == target)
                 {
-                    sol.Add(i);
-                    sol.Add(j);
-                    return sol.ToArray();
+                    return new int[] {i,j};
                 }
             }
         }
-        return sol.ToArray() ;
+        return new int[0] ;
     }
 }
