c7551a0b3f9d1ec8c65f531978d768310d2800f9
NithishVaran-S <133568626+NithishVaran-S@users.noreply.github.com>
2026-05-09 23:54:22 +0530

    Time: 71 ms (11.41%) | Memory: 57 MB (8.93%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+    
+        int big = 0; 
+        
+
+        for (int i = 0; i < s.Length; i++)
+        {   
+            int count = 0;
+            HashSet<char> check = new HashSet<char>();
+            for (int j = i; j < s.Length; j++)
+            {
+                if (check.Contains(s[j])){
+                    break;
+                }
+                else{
+                    count += 1;
+                    check.Add(s[j]);
+                }
+                if (count > big){
+                    big = count;
+                }
+            }
+        }
+        return big;
+    }
+}
